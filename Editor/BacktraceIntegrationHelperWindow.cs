08f26fd0b713d649b9da6901c2cdf1f45b469b69
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2021-03-17 15:19:45 -0400

    IntegrationHelperEditor

---
+++ Editor/BacktraceIntegrationHelperWindow.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using Backtrace.Unity;
+using Backtrace.Unity.Model;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Backtrace.Unity.Editor
+{
+    public class BacktraceIntegrationHelperWindow : EditorWindow
+    {
+        // Constant Labels and URLs
+        private const string LogoPath = "Assets/Art/Backtrace Logo/PNGs/Logo-Backtrace-TrueBlack-Horiz.png";
+
+        // Notifications / error messages
+        private string windowLogMessage;
+        private MessageType logMessageType;
+        
+        private Vector2 scrollPosition;
+        private Texture logo;
+        
+        private UnityEditor.Editor backtraceConfigurationEditor;
+
+        // User input variables
+        [SerializeField] string pathToCreateScriptable = "Assets/Backtrace/";
+        [SerializeField] string configFileName = "BacktraceConfiguration";
+        [SerializeField] private BacktraceConfiguration backtraceConfiguration;
+
+        private GameObject clientGameObject;
+        
+        /// <summary>
+        /// Creates the Editor window.  Accessible via Unity's top toolbar under Window>>Backtrace.
+        /// </summary>
+        /// <returns></returns>
+        [MenuItem("Window/Backtrace/IntegrationHelper")]
+        public static BacktraceIntegrationHelperWindow ShowWindow()
+        {
+            BacktraceIntegrationHelperWindow window =
+                (BacktraceIntegrationHelperWindow) EditorWindow.GetWindow(typeof(BacktraceIntegrationHelperWindow), 
+                    false, BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_WINDOW_TITLE);
+
+            window.minSize = new Vector2(400, 500);
+            window.Show();
+            return window;
+        }
+
+        #region Unity Callbacks
+
+        private void OnEnable()
+        {
+            // Basic variable initialization
+            logo = AssetDatabase.LoadAssetAtPath<Texture>(LogoPath);
+            logMessageType = MessageType.Info;
+            windowLogMessage = BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_WINDOW_WELCOME_MESSAGE;
+            scrollPosition = new Vector2(0, 0);
+        }
+
+        
+        private void OnGUI()
+        {
+            // Header
+            BTEditorUtility.DrawHeader(logo, Color.white, position);
+            BTEditorUtility.DrawHorizontalUILine(Color.black,2,1);
+            
+            // Documentation Links
+            EditorGUILayout.LabelField(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_RESOURCELINKSSECTION_HEADER, 
+                BTEditorUtility.HeaderTextStyle);
+            DrawDocumentationButtons();
+            BTEditorUtility.DrawHorizontalUILine(Color.black,2,1);
+            GUILayout.Space(10);
+            
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            
+            // Config asset creation + manipulation
+            DrawBacktraceConfigSections();
+            BTEditorUtility.DrawHorizontalUILine(Color.black,2,1);
+            GUILayout.Space(10);
+            
+            // Client creation
+            DrawBacktraceClientSection();
+            
+            EditorGUILayout.EndScrollView();
+
+            // Notification box
+            DrawNotificationMessage();
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        void DrawBacktraceClientSection()
+        {
+            EditorGUILayout.LabelField(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_CLIENTSECTION_HEADER, BTEditorUtility.HeaderTextStyle);
+            BTEditorUtility.DrawHorizontalUILine(Color.grey,2,1);
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_CREATECLIENT_BUTTON, BTEditorUtility.BaseFieldSizingLayoutOptions))
+            {
+                if (backtraceConfiguration == null)
+                {
+                    logMessageType = MessageType.Warning;
+                    windowLogMessage = "Failed to create new Backtrace Client because you do not have a configuration selected. " +
+                                       "Please create or select a Backtrace Configuration asset and try again.";
+                }
+                else
+                {
+                    if (clientGameObject == null)
+                    {
+                        clientGameObject = new GameObject("BacktraceClient", typeof(BacktraceClient));
+                        clientGameObject.GetComponent<BacktraceClient>().Configuration = backtraceConfiguration;
+                        logMessageType = MessageType.Info;
+                        windowLogMessage = "Backtrace Client created in Scene: " + SceneManager.GetActiveScene().name;
+                    }
+                    else
+                    {
+                        clientGameObject.GetComponent<BacktraceClient>().Configuration = backtraceConfiguration;
+                        logMessageType = MessageType.Warning;
+                        windowLogMessage = "Backtrace Client already exists in Scene: " + SceneManager.GetActiveScene().name + 
+                                           "\nThe existing Client was updated to reflect the currently selected Configuration";
+                    }
+                    
+                    EditorUtility.FocusProjectWindow();
+                    Selection.activeObject = clientGameObject;
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        void DrawBacktraceConfigSections()
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_CONFIGSECTION_HEADER, BTEditorUtility.HeaderTextStyle);
+            GUILayout.FlexibleSpace();
+
+            backtraceConfiguration = (BacktraceConfiguration)EditorGUILayout
+                .ObjectField(backtraceConfiguration, typeof(BacktraceConfiguration), new GUILayoutOption[]
+                {
+                    GUILayout.MinWidth(position.width / 3)
+                });
+
+            EditorGUILayout.EndHorizontal();
+            BTEditorUtility.DrawHorizontalUILine(Color.grey,2,1);
+            if (backtraceConfiguration != null)
+            {
+                BTEditorUtility.DrawSubHeading("Settings for: " + backtraceConfiguration.name);
+
+                if (backtraceConfigurationEditor == null)
+                    backtraceConfigurationEditor = UnityEditor.Editor.CreateEditor(backtraceConfiguration);
+                backtraceConfigurationEditor.OnInspectorGUI();
+                backtraceConfigurationEditor.Repaint();
+            }
+            else
+            {
+                DrawConfigCreatorSection();
+            }
+        }
+
+        void DrawConfigCreatorSection()
+        {
+            BTEditorUtility.DrawSubHeading(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_CONFIGCREATIONSECTION_SUBHEADER);
+            
+            // Buttons
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_CONFIGCREATION_BUTTON, 
+                BTEditorUtility.DefineLayoutSizingConstraints(10,30,60,position.width / 2)))
+            {
+                string fullPath = pathToCreateScriptable + configFileName + ".asset";
+                if (File.Exists(fullPath))
+                {
+                    windowLogMessage = fullPath + " already exists! No configuration asset was created.";
+                    logMessageType = MessageType.Warning;
+                    return;
+                }
+
+                if (!Directory.Exists(pathToCreateScriptable))
+                {
+                    Directory.CreateDirectory(pathToCreateScriptable);
+                    windowLogMessage = pathToCreateScriptable + " directory created.\n" + fullPath + " created successfully!";
+                }
+                else
+                {
+                    windowLogMessage = fullPath + " created successfully!";
+                }
+                logMessageType = MessageType.Info;
+                
+                BacktraceConfiguration configAsset = ScriptableObject.CreateInstance<BacktraceConfiguration>();
+                AssetDatabase.CreateAsset(configAsset, fullPath);
+                AssetDatabase.SaveAssets();
+                
+                EditorUtility.FocusProjectWindow();
+                Selection.activeObject = configAsset;
+                backtraceConfiguration = configAsset;
+                backtraceConfigurationEditor = UnityEditor.Editor.CreateEditor(backtraceConfiguration);
+            }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            
+            
+            // Text fields
+            GUILayout.BeginHorizontal();
+            
+            GUILayout.BeginVertical();
+            EditorStyles.textField.wordWrap = true;
+            GUILayout.Label(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_CONFIGNAME);
+            configFileName = GUILayout.TextField(configFileName, 
+                BTEditorUtility.DefineLayoutSizingConstraints(10,20,60,600));
+            GUILayout.EndVertical();
+            
+            GUILayout.BeginVertical();
+            GUILayout.Label(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_ASSETPATH);
+            
+            pathToCreateScriptable = GUILayout.TextField(pathToCreateScriptable,
+                BTEditorUtility.DefineLayoutSizingConstraints(10, 20, 60, 600));
+            EditorStyles.textField.wordWrap = false;
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+        }
+
+        void DrawNotificationMessage()
+        {
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.HelpBox(windowLogMessage, logMessageType);
+        }
+
+        void DrawDocumentationButtons()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            
+            if (GUILayout.Button(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_DOCUMENTATION_BUTTON, 
+                BTEditorUtility.DefineLayoutSizingConstraints(10,20,60,600))) 
+            { OnDocumentationButtonPressed(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_DOCUMENTATION_URL); }
+
+            if (GUILayout.Button(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_VIDEO_BUTTON, 
+                BTEditorUtility.DefineLayoutSizingConstraints(10,20,60,600))) 
+            { OnDocumentationButtonPressed(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_VIDEO_URL);}
+
+            if (GUILayout.Button(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_SUPPORT_BUTTON, 
+                BTEditorUtility.DefineLayoutSizingConstraints(10,20,60,600)))
+            { OnDocumentationButtonPressed(BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_SUPPORT_URL);}
+ 
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        void OnDocumentationButtonPressed(string url)
+        {
+            Application.OpenURL(url);
+            windowLogMessage = BacktraceIntegrationWindowLabels.LABEL_INTEGRATION_WINDOW_WELCOME_MESSAGE;
+            logMessageType = MessageType.Info;
+        }
+
+        #endregion
+        
+
+    }
+}
---
+++ Editor/BacktraceIntegrationWindowLabels.cs
@@ -0,0 +1,30 @@
+﻿namespace Backtrace.Unity.Editor
+{
+    internal static class BacktraceIntegrationWindowLabels
+    {
+        internal static string LABEL_INTEGRATION_WINDOW_TITLE = "Backtrace";
+        internal static string LABEL_INTEGRATION_RESOURCELINKSSECTION_HEADER = "Documentation Resources";
+
+        internal static string LABEL_INTEGRATION_DOCUMENTATION_URL = 
+            "https://support.backtrace.io/hc/en-us/articles/360040515991-Unity-Integration-Guide";
+
+        internal static string LABEL_INTEGRATION_VIDEO_URL = 
+            "https://backtrace.io/backtrace-resources/video/";
+
+        internal static string LABEL_INTEGRATION_SUPPORT_URL = "https://backtrace.io/support/";
+
+        internal static string LABEL_INTEGRATION_WINDOW_WELCOME_MESSAGE = 
+            "Welcome to Backtrace-Unity! Use this window to get started or refer to our documentation linked above";
+
+        internal static string LABEL_INTEGRATION_DOCUMENTATION_BUTTON = "Integration Guide";
+        internal static string LABEL_INTEGRATION_VIDEO_BUTTON = "Videos";
+        internal static string LABEL_INTEGRATION_SUPPORT_BUTTON = "Support";
+        internal static string LABEL_INTEGRATION_CONFIGSECTION_HEADER = "Backtrace Configuration Setup";
+        internal static string LABEL_INTEGRATION_CONFIGCREATIONSECTION_SUBHEADER = "Backtrace Config Creator";
+        internal static string LABEL_INTEGRATION_CONFIGCREATION_BUTTON = "Create New Config Asset";
+        internal static string LABEL_INTEGRATION_CLIENTSECTION_HEADER = "Backtrace Client Setup";
+        internal static string LABEL_INTEGRATION_CREATECLIENT_BUTTON = "Initialize Backtrace Client with Current Config";
+        internal static string LABEL_INTEGRATION_CONFIGNAME = "Config Name";
+        internal static string LABEL_INTEGRATION_ASSETPATH = "Asset Path";
+    }
+}
